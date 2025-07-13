08b044cf33f9558cfa83d300dd9d1aafd2b9d287
rahim707 <mazouzceminfo@gmail.com>
2025-07-13 14:49:35 +0200

    change the directory

--- app/models/models.py
+++
@@ -1,9 +0,0 @@
-'''
-Usually refer to data structures that represent database tables or entities.
-
-In ORMs (like SQLAlchemy, Django ORM), models define the shape of your database records — their fields, types, relations.
-
-Models are tied to persistence, meaning they map to how data is stored and retrieved.
-
-Example: A User model with fields like id, email, hashed_password, stored in a database.
-'''
