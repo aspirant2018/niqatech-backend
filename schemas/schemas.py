b4b5bb6cdcc234ca51056d85b5b5d389ed2782f4
rahim707 <mazouzceminfo@gmail.com>
2025-07-19 10:52:58 +0200

    add reponse model for uploading file

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 import enum
 
 
@@ -28,3 +29,32 @@ class ItemResponse(BaseModel):
     email: str
     is_profile_complete: bool
     jwt_token: str
+
+
+
+
+class Student(BaseModel):
+    id: int
+    row: int
+    last_name: str
+    first_name: str
+    date_of_birth: str  # or date if you parse it
+    evaluation: Optional[float]
+    first_assignment: Optional[float]
+    final_exam: Optional[float]
+    observation: Optional[str]
+
+class Classroom(BaseModel):
+    school_name: str
+    term: str
+    year: str
+    level: str
+    subject: str
+    classroom_id: str
+    classroom_name: str
+    number_of_students: int
+    students: List[Student]
+
+class WorkbookParseResponse(BaseModel):
+    message: str = "XLS file parsed successfully"
+    data: dict[str, List[Classroom]]  # {"classrooms": [...]}
