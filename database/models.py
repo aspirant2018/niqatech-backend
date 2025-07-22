fd3a4b122f8768bb85f5a92515c81c0d87b36474
rahim707 <mazouzceminfo@gmail.com>
2025-07-22 12:02:37 +0200

    update the class models

--- database/models.py
+++ database/models.py
@@ -10,7 +10,7 @@ Example: A User model with fields like id, email, hashed_password, stored in a d
 
 
 from sqlalchemy import Column, Integer, String, Boolean, Enum, Float, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from schemas.schemas import AcademicLevelEnum
 import uuid
 from sqlalchemy.orm import declarative_base
@@ -33,6 +33,9 @@ class User(Base):
     city = Column(String, nullable=False)
     subject = Column(String, nullable=False)
 
+    file = relationship("UploadedFile", back_populates="user", uselist=False) # Uselist => Ensures it's a one-to-one relationship, Back populate => biderectional
+
+
     def __repr__(self):
         return f"<teacher(id={self.id}, email={self.email}, first_name={self.first_name}, last_name={self.last_name})>"
 
@@ -42,8 +45,54 @@ class UploadedFile(Base):
     file_id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, nullable=False)
     user_id = Column(String, ForeignKey(User.id), unique=True, nullable=False)
     file_name = Column(String, nullable=False)
+    user = relationship("User", back_populates="file")
 
 
     def __repr__(self):
         return f"<file(file_id={self.file_id}, user_id={self.user_id}, file_name={self.file_name})>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Classroom(Base):
+    __tablename__ = "classrooms"
+
+    classroom_id = Column(String, primary_key=True) 
+    file_id = Column(String, ForeignKey(UploadedFile.file_id), unique=True, nullable=False)
+
+class Student(Base):
+    __tablename__ = "students"
+
+    student_id = Column(String, primary_key=True)
+    row = Column(Integer, nullable = False)
+    last_name = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    date_of_birth = Column(String, nullable=False)
+
+    evaluation = Column(Float)
+    first_assignment = Column(String)
+    final_exam = Column(String)
+    observation = Column(String)
+
+
+
+
+
+
 
