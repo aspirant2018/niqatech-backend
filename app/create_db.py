f9f179ad23895c0ef437bb4398d993d64d32f86c
rahim707 <mazouzceminfo@gmail.com>
2025-09-09 15:51:57 +0200

    move create and main into app dir

--- app/create_db.py
+++ app/create_db.py
@@ -7,7 +7,8 @@ _ = models.User()
 _ = models.UploadedFile()
 
 
-print("Creating database and tables...")
+print("Deleting database and tables...")
 Base.metadata.drop_all(bind=engine)   # <-- supprime toutes les tables
+print("Creating database and tables...")
 Base.metadata.create_all(bind=engine)
 print("Done.")
