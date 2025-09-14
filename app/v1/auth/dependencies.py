df2dd96802a7c5f7b066889b02a192e87f9f7d38
rahim707 <mazouzceminfo@gmail.com>
2025-09-14 20:27:13 +0200

    log the jwt token

--- app/v1/auth/dependencies.py
+++ app/v1/auth/dependencies.py
@@ -1,13 +1,25 @@
 from fastapi import Request, HTTPException, status
 from jose import jwt, JWTError
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger("__dependencies.py__")
 
 
 def get_current_user(request: Request):
     """
     Extracts the user ID from the JWT token in the request headers.
-    This function is used as a dependency in FastAPI routes to ensure that the user is authenticated
+    This function is used as a dependency in FastAPI routes to ensure that the user is authorized.
     """
+    logger.info(f"Headers: {request.headers}")
+    logger.info(f"Authorization Header: {request.headers.get('Authorization')}")
+
+    
     auth_header = request.headers.get("Authorization")
+
     if not auth_header or not auth_header.startswith("Bearer"):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
