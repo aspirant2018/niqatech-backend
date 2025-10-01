a3ee17f4c17597755c08b8f4454749a3589f319e
rahim707 <mazouzceminfo@gmail.com>
2025-10-01 13:52:36 +0200

    refactor: clear the code

--- app/v1/routers/auth.py
+++ app/v1/routers/auth.py
@@ -23,9 +23,9 @@ logging.basicConfig(
 logger = logging.getLogger("__routers/auth.py__")
 
 
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "245808035770-5e2rf7c0a5kqcfd6d7q4h9r0car8mttc.apps.googleusercontent.com")
-SECRET_KEY = os.getenv("SECRET_KEY", "1234")  # Use a secure key in production
-ALGORITHM = os.getenv("ALGORITHM", "HS256")  # Use a secure algorithm
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+SECRET_KEY = os.getenv("SECRET_KEY")  # Use a secure key in production
+ALGORITHM = os.getenv("ALGORITHM")  # Use a secure algorithm
 
 
 
@@ -37,11 +37,13 @@ router = APIRouter(
 
 
 # localhost:8000/auth/google
+# called in signin component in frontend
+
 @router.post("/google", response_model=ItemResponse)
 async def google_auth(token_data: TokenData, db: Session = Depends(get_db)):
     """ Authenticate user via Google OAuth token."""
 
-    logger.info(f"Google token received from frontend, The Token: {token_data}")
+    #logger.info(f"Google token received from frontend, The Token: {token_data}")
 
     try:
         id_info = id_token.verify_oauth2_token(
@@ -61,43 +63,28 @@ async def google_auth(token_data: TokenData, db: Session = Depends(get_db)):
 
         logger.info(f"User ID: {user_id}, Email: {email}")
         
-        users = db.query(User).all()
-        logger.info(f"Number of users in the database: {len(users)}")
 
         app_jwt_token = generate_jwt_token(user_id, SECRET_KEY, ALGORITHM)
-        user = db.query(User).filter(User.id == user_id).first()
-
-        logger.info(f"Users {users}")  # Ensure the database is connected
-        logger.info(f"JWT token generated: {app_jwt_token}")
-        logger.info(f"The user retrieved from the database: {user}")
-
-        # User is in the database
-        if user:
-            logger.info(f"User with ID {user_id} found in the database.")
-            return JSONResponse(
-                status_code=400,
-                content={"message": "User already exists"}
-            )
-            #return {
-            #    "message": "User authenticated successfully",
-            #    "user_id": user_id,
-            #    "email": email,
-            #    "first_name": user.first_name,
-            #    "last_name": user.last_name,
-            #    "is_profile_complete": True,
-            #    "jwt_token": app_jwt_token,
-            #}
-
-        logger.info(f"User with ID {user_id} not found in the database. Creating a new user.")
-        return {
+        user = db.query(User).filter(User.email == email).one_or_none()
+        # User is not in the database
+        if not user:
+            logger.info(f"User with ID {user_id} not found in the database.")
+            return {
                 "message": "User first login. Please complete the profile.",
                 "user_id": user_id,
                 "email": email,
                 "is_profile_complete": False,
                 "jwt_token": app_jwt_token
-
                 }
-
+        
+        # User is in the database
+        if user:
+            logger.info(f"User with ID {user_id} found in the database.")
+            return JSONResponse(
+                status_code=status.HTTP_409_CONFLICT,
+                content={"message": "User already exists"}
+            )
+        
     except ValueError:
         raise HTTPException(status_code=401, detail="Invalid ID token")
 
@@ -127,7 +114,6 @@ async def login(token_data: TokenData, db: Session = Depends(get_db)):
         email   = id_info['email']
 
         logger.info(f"User ID: {user_id}, Email: {email}")
- 
 
         if not user_id:
             raise HTTPException(status_code=401, detail="Invalid token")
@@ -138,8 +124,6 @@ async def login(token_data: TokenData, db: Session = Depends(get_db)):
             raise HTTPException(status_code=404, detail="User not found")
 
         logger.info(f"User {user.email} logged in successfully.")
-
-        
 
         return {
             "message": "User logged in successfully",
