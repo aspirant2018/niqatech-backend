35f6dc405fbc42ddd0ef44ab4e7fdb5363a93623
rahim707 <mazouzceminfo@gmail.com>
2025-10-11 12:29:16 +0200

    refactor: modify create_access_token

--- app/v1/auth/jwt_utils.py
+++ app/v1/auth/jwt_utils.py
@@ -1,13 +1,19 @@
 from jose import jwt
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")  # Use a secure key in production
+ALGORITHM = os.getenv("ALGORITHM")  # Use a secure algorithm
+
 
 # Function to generate a JWT token
-def generate_jwt_token(user_id: str, SECRET_KEY, ALGORITHM):
+def create_access_token(data):
     """
     Generate a JWT token for the authenticated user.
     This is a placeholder function; implement JWT generation logic here.
     """
-    payload = {
-        "sub": user_id,
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    to_enocde = data.copy()
+    # add expiration time if needed
+    # to_enocde.update({"exp": datetime.utcnow() + timedelta(minutes=15)})
+
+    token = jwt.encode(to_enocde, SECRET_KEY, algorithm=ALGORITHM)
     return token
